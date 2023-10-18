f469464ea1f24c3f63d9020898a2c83249f2faaa
leonid <bertre.com@gmail.com>
2023-10-18 13:03:21 +0300

    Замена отображения языка и часового пояса.

--- answer_me/settings.py
+++ answer_me/settings.py
@@ -104,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Etc/GMT-3'
 
 USE_I18N = True
 
