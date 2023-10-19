d382175f4a8042e3026096670e31b66947d71216
leonid <bertre.com@gmail.com>
2023-10-19 12:09:51 +0300

    Создание моделей для вопросов и ответов в models.py, создание миграции для отображения таблиц в базе данных, регистрация и настройка админ-панели в admin.py, создание суперпользователя.

--- questions/admin.py
+++ questions/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Question, Choice
+
+# Настройка отображения административной панели.
+admin.site.site_header = 'Администратор'
+admin.site.site_title = 'Админ-панель'
+admin.site.index_title = 'Административная зона'
+
+
+class ChoiceInLine(admin.TabularInline):
+    model = Choice
+    extra = 4
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [(None, {"fields": ["question_text"]}), ('Дата добавления', {
+        "fields": ["pub_date"], "classes": ["collapse"]}), ]
+    inlines = [ChoiceInLine]
+
+
+admin.site.register(Question, QuestionAdmin)
--- questions/models.py
+++ questions/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+# Вопросы.
+class Question(models.Model):
+    question_text = models.CharField(max_length=255, verbose_name='Вопрос')
+    pub_date = models.DateTimeField('дата публикации')
+
+    def __str__(self):
+        return self.question_text
+
+    class Meta:
+        verbose_name = 'Вопрос'
+        verbose_name_plural = 'Вопросы'
+
+
+# Ответ.
+class Choice(models.Model):
+    # Связь один к одному.
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=255, verbose_name='Ответ')
+    votes = models.IntegerField(default=0, verbose_name='голоса')
+
+    def __str__(self):
+        return self.choice_text
+
+    class Meta:
+        verbose_name = 'Ответ'
+        verbose_name_plural = 'Ответы'
